4e8381f1af2932423a7b515c34ba8c0c4701185c
Vasagirisravani <123653273+Vasagirisravani@users.noreply.github.com>
2023-03-28 22:44:30 +0530

    Add files via upload

---
+++ COMSPION IN NUMBERS.cpp
@@ -0,0 +1,20 @@
+// C# program to print
+// table of a number
+using System;
+ 
+class GFG
+{
+    // Driver code
+    public static void Main()
+    {
+        // Change here to
+        // change output
+        int n = 5;
+         
+        for (int i = 1; i <= 10; ++i)
+            Console.Write(n + " * " + i +
+                              " = " + n *
+                               i + "\n");
+    }
+}
+
